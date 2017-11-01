c4aeab63e9d273e1bf7fe24bb235d518e6901c83
TheSaminator <samfrancis00@gmail.com>
2017-11-01 18:57:03 -0400

    Add start of changes for tele-op strategy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotTeleop.java
@@ -2,17 +2,33 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by The Saminator on 10-01-2017.
  */
 @TeleOp(name = "Drivetrain Tele-Op Test", group = "SPECIALMAN")
 public class DriveBotTeleop extends DriveBotTemplate {
     private boolean prevA;
+    private Timer async;
+
+    private boolean lifting;
 
     @Override
     public void init() {
         super.init();
         prevA = false;
+    }
+
+    protected void highDelivery() {
+        lifting = true;
+        async.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                setLift2Pow(0.5);
+            }
+        }, 0);
     }
 
     @Override
@@ -22,6 +38,8 @@ public class DriveBotTeleop extends DriveBotTemplate {
 
         if (gamepad1.a && !prevA)
             scream();
+
+
 
         prevA = gamepad1.a;
     }
