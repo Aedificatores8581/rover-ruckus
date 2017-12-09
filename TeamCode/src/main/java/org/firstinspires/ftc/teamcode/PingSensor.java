126994adba83ec8c9b5d82d1ee9d0c52c8cd1f5f
TheSaminator <samfrancis00@gmail.com>
2017-12-09 14:47:52 -0500

    Fix possible bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PingSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PingSensor.java
@@ -2,12 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
+import java.util.concurrent.locks.ReadWriteLock;
+
 /**
  * Conjured into existence by The Saminator on 12-09-2017.
  */
 public class PingSensor {
     private DigitalChannel channel;
+    private final ReadWriteLock readConcurrency = new java.util.concurrent.locks.ReentrantReadWriteLock();
     private long lastReading;
+    private boolean readingFresh;
 
     public static final double SPEED_OF_SOUND_MM_PER_NS = 0.000343;
     public static final double SPEED_OF_SOUND_CM_PER_NS = 0.0000343;
@@ -16,14 +20,18 @@ public class PingSensor {
     public PingSensor(DigitalChannel channel) {
         this.channel = channel;
         lastReading = 0;
+        readingFresh = false;
     }
 
     public void startReading() {
-        lastReading = 0;
+        readingFresh = false;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 long timePulsed;
+
+                readConcurrency.writeLock().lock();
+                lastReading = 0;
 
                 channel.setMode(DigitalChannel.Mode.OUTPUT);
                 channel.setState(true);
@@ -34,23 +42,33 @@ public class PingSensor {
                 while (channel.getState());
 
                 lastReading = System.nanoTime() - timePulsed;
+                readConcurrency.writeLock().unlock();
+
+                readingFresh = true;
             }
         }).start();
     }
 
+    public boolean isReadingFresh() {
+        return readingFresh;
+    }
+
     public long getLastReadingNanoseconds() {
-        return lastReading;
+        readConcurrency.readLock().lock();
+        long value = lastReading;
+        readConcurrency.readLock().unlock();
+        return value;
     }
 
     public double getLastReadingMillimeters() {
-        return lastReading * SPEED_OF_SOUND_MM_PER_NS;
+        return getLastReadingNanoseconds() * SPEED_OF_SOUND_MM_PER_NS;
     }
 
     public double getLastReadingCentimeters() {
-        return lastReading * SPEED_OF_SOUND_CM_PER_NS;
+        return getLastReadingNanoseconds() * SPEED_OF_SOUND_CM_PER_NS;
     }
 
     public double getLastReadingInches() {
-        return lastReading * SPEED_OF_SOUND_IN_PER_NS;
+        return getLastReadingNanoseconds() * SPEED_OF_SOUND_IN_PER_NS;
     }
 }
