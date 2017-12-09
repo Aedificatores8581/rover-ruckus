2803a1d8d69de8aa031fb397c4300326c840c741
TheSaminator <samfrancis00@gmail.com>
2017-12-08 19:28:24 -0500

    Add debug message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor2BotODSTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor2BotODSTest.java
@@ -6,20 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  * Created by Hunter Seachrist on 12/5/2017.
  */
 @TeleOp(name = "SensorBot: Opical Distance Sensor Test", group = "bepis")
-public class Sensor2BotODSTest extends Sensor2BotTemplate{
+public class Sensor2BotODSTest extends Sensor2BotTemplate {
 
     private enum State {SETTING_DISTANCE, DRIVING, STOPPED}
+
     State state;
-    double lightAmount;
 
-
-    public void init(){
+    public void init() {
         state = State.SETTING_DISTANCE;
     }
 
-    public void loop(){
-        lightAmount = ods.getLightDetected();
-        telemetry.addData("Light", lightAmount);
+    public void loop() {
+
+        if (ods == null)
+            telemetry.addData("ERROR ERROR ERROR", "OPTICAL DISTANCE SENSOR IS NULL THIS IS VERY BAD REQUESTING TO ABORT REPEAT REQUESTING TO ABORT");
+        else
+            telemetry.addData("Light", ods.getLightDetected());
 
     }
 }
