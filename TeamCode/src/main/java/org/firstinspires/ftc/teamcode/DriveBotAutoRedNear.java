991afc9ded2f6b04fbad61ee0bccda3f1d3c99aa
TheSaminator <samfrancis00@gmail.com>
2017-12-30 14:02:00 -0500

    Fix NullPointerException

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotAutoRedNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotAutoRedNear.java
@@ -76,11 +76,12 @@ public class DriveBotAutoRedNear extends DriveBotTestTemplate {
 
     @Override
     public void init() {
+        super.init();
+        state = State.STATE_LOWER_JEWEL_ARM;
+
         rIntake.setPosition(0.3);
 
         lIntake.setPosition(0.3);
-        super.init();
-        state = State.STATE_LOWER_JEWEL_ARM;
 
         cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
