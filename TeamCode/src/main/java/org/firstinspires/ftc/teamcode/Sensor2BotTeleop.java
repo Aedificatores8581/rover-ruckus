0fb7af8a7ea57294706a351dc729cacdad3a13a9
TheSaminator <samfrancis00@gmail.com>
2017-12-30 14:49:00 -0500

    Add relic arm to SensorBot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor2BotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor2BotTeleop.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
@@ -15,7 +14,23 @@ public class Sensor2BotTeleop extends Sensor2BotTemplate {
 
     @Override
     public void loop() {
+        double relicHandPos = relicHand.getPosition();
+        double relicFingersPos = relicFingers.getPosition();
+
         lm.setPower(gamepad1.left_stick_y * Constants.MOTOR_POWER);
         rm.setPower(gamepad1.right_stick_y * Constants.MOTOR_POWER);
+
+        relicArm.setPower(gamepad2.right_stick_y);
+
+        if (triggered(Math.abs(gamepad2.left_stick_y)))
+            relicHandPos += gamepad2.left_stick_y * Constants.RELIC_HAND_DELTA_MULT;
+
+        if (triggered(gamepad2.right_trigger))
+            relicFingersPos += Constants.RELIC_FINGERS_DELTA;
+        if (triggered(gamepad2.left_trigger))
+            relicFingersPos -= Constants.RELIC_FINGERS_DELTA;
+
+        relicHand.setPosition(relicHandPos);
+        relicFingers.setPosition(relicFingersPos);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor2BotTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor2BotTemplate.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Const;
@@ -18,11 +19,19 @@ public abstract class Sensor2BotTemplate extends OpMode {
     protected PingSensor ping;
     protected SharpIRSensor ir;
     protected DcMotor lm, rm;
+    protected Servo relicHand, relicFingers; // rah, raf
+    protected DcMotor relicArm;
 
     public static class Constants {
         public static final double MOTOR_POWER = 0.25;
+
+        public static final double RELIC_MOTOR_POWER = 0.5;
+        public static final double RELIC_HAND_DELTA_MULT = 0.125;
+        public static final double RELIC_FINGERS_DELTA = 0.0625;
+
         public static final DcMotor.Direction LM_DIR = DcMotorSimple.Direction.FORWARD;
         public static final DcMotor.Direction RM_DIR = DcMotorSimple.Direction.REVERSE;
+        public static final DcMotor.Direction RA_DIR = DcMotorSimple.Direction.REVERSE;
     }
 
     @Override
@@ -35,9 +44,14 @@ public abstract class Sensor2BotTemplate extends OpMode {
         lm = hardwareMap.dcMotor.get("lm");
         rm = hardwareMap.dcMotor.get("rm");
 
+        relicArm = hardwareMap.dcMotor.get("ram");
+        relicHand = hardwareMap.servo.get("rah");
+        relicFingers = hardwareMap.servo.get("raf");
+
         touch.setMode(DigitalChannel.Mode.INPUT);
         lm.setDirection(Constants.LM_DIR);
         rm.setDirection(Constants.RM_DIR);
+        relicArm.setDirection(Constants.RA_DIR);
     }
 
     public void go() {
@@ -48,5 +62,9 @@ public abstract class Sensor2BotTemplate extends OpMode {
     public void stop() {
         lm.setPower(0.0);
         rm.setPower(0.0);
+    }
+
+    protected boolean triggered(double value) {
+        return value >= 0.05;
     }
 }
