ad07dd99f9c70144a2671c12256f742a4352b324
theZebraBatman <fgportman00@gmail.com>
2018-02-09 20:55:35 -0500

    Merge branch 'master' of https://github.com/Aedificatores8581/ftc_app
    # Conflicts:
    #	build.gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmpSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmpSensorTest.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 /**
  * Created by Hunter Seachrist on 2/9/2018.
  */
@@ -16,9 +16,14 @@ public class AmpSensorTest extends OpMode {
     Servo testServo;
     double servoPosition;
 
+    protected DigitalChannel magFront, magBack;
+
+
     public void init(){
         ampSensor = hardwareMap.get(AnalogInput.class, "amp");
         testServo = hardwareMap.servo.get("servo");
+        magFront = hardwareMap.digitalChannel.get("mf");
+        magBack = hardwareMap.digitalChannel.get("mb");
     }
 
 
@@ -32,6 +37,11 @@ public class AmpSensorTest extends OpMode {
         else if (gamepad1.right_bumper) {
             servoPosition -= 0.02;
         }
+        telemetry.addData("back.getState:  ", magBack.getState());
+        telemetry.addData("back.getMode:   ", magBack.getMode());
+        telemetry.addData("front.getState:  ", magFront.getState());
+        telemetry.addData("front.getMode:   ", magFront.getMode());
+
 
     }
 }
