df7ba8b98d3cc459a22021863cb5c48cfe5a23ef
Mister-Minister-Master <misterministermaster@gmail.com>
2018-02-09 20:34:20 -0500

    AmpSensorTest things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmpSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AmpSensorTest.java
@@ -1,22 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.AnalogSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Hunter Seachrist on 2/9/2018.
  */
 
+@TeleOp(name = "SensorBot: Amp Sensor", group = "Sensor Test")
 public class AmpSensorTest extends OpMode {
-    AnalogSensor ampSensor;
+    AnalogInput ampSensor;
+    Servo testServo;
+    double servoPosition;
 
     public void init(){
-        ampSensor = hardwareMap.get(AnalogSensor.class, "amp");
-
+        ampSensor = hardwareMap.get(AnalogInput.class, "amp");
+        testServo = hardwareMap.servo.get("servo");
     }
 
 
     public void loop(){
-        telemetry.addData("Amp Sensor Voltage", ampSensor.readRawVoltage());
+        testServo.setPosition(servoPosition);
+
+        telemetry.addLine("Amp Sensor Voltage: " + ampSensor.getVoltage());
+        if (gamepad1.left_bumper) {
+            servoPosition += 0.02;
+        }
+        else if (gamepad1.right_bumper) {
+            servoPosition -= 0.02;
+        }
+
     }
 }
