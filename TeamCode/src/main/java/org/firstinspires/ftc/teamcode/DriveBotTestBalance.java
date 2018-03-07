cc58bdc1baecda2548acdd9dbf106098551f995b
TheSaminator <samfrancis00@gmail.com>
2018-03-06 20:27:28 -0500

    Port balance modes to DriveBot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotTestBalance.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+
+/**
+ * Conjured into existence by The Saminator on 03-06-2018.
+ */
+@Autonomous(name = "DriveBot Balance", group = "this is a test")
+public class DriveBotTestBalance extends DriveBotTestTemplate {
+
+    static class Angles {
+        public static final double Y_TOLERANCE = 0.375; // Actually the tolerance for sin(theta).
+        public static final double PHI_BASELINE = 0;
+        public static final double PHI_TOLERANCE = 3.75;
+        public static final double THETA_BASELINE = 0;
+        public static final double THETA_TOLERANCE = 3.75;
+
+        public static boolean withinPhiLimits(double phi) {
+            return Utilities.withinTolerance(phi, Angles.PHI_BASELINE, Angles.PHI_TOLERANCE);
+        }
+
+        public static boolean withinThetaLimits(double theta) {
+            return Utilities.withinTolerance(theta, Angles.THETA_BASELINE, Angles.THETA_TOLERANCE);
+        }
+    }
+
+    Acceleration gravity;
+    Spherical3D angles;
+
+    //--------------------------------------------------------------------------
+    //
+    // loop
+    //
+    //-------
+    // Initializes the class.
+    //
+    // The system calls this member repeatedly while the OpMode is running.
+    //--------
+    @Override
+    public void loop() {
+        gravity = imu.getGravity();
+        angles = cartesianToSpherical(new Cartesian3D(gravity.zAccel, gravity.xAccel, gravity.yAccel));
+
+        telemetry.addData("(NAV) Status", imu.getSystemStatus().toShortString());
+        telemetry.addData("(NAV) Calib.", imu.getCalibrationStatus());
+
+        telemetry.addData("Theta angle ( acos (z / sqrt(x^2 + y^2 + z^2)) )", angles.theta);
+        telemetry.addData("Phi angle ( atan (y / x) )", angles.phi);
+        telemetry.addData("X gravity", gravity.xAccel);
+        telemetry.addData("Y gravity", gravity.yAccel);
+        telemetry.addData("Z gravity", gravity.zAccel);
+
+        if (angles.theta > Angles.THETA_TOLERANCE) {
+            double leftPow = 0.125;
+            double rightPow = 0.125;
+            // Account for fore/back tilt
+            double foreBack = Math.sin(org.firstinspires.ftc.teamcode.Constants.DEGS_TO_RADS * (angles.theta - Angles.THETA_BASELINE)); // * Math.cos(org.firstinspires.ftc.teamcode.Constants.DEGS_TO_RADS * (angles.phi - Angles.PHI_BASELINE));
+            foreBack += Math.abs(foreBack) / foreBack;
+            leftPow *= foreBack;
+            rightPow *= foreBack;
+            /*// Account for left/right tilt
+            double sinPhi = Math.sin(org.firstinspires.ftc.teamcode.Constants.DEGS_TO_RADS * (angles.phi - Angles.PHI_BASELINE));
+            if (Math.abs(sinPhi) >= Angles.Y_TOLERANCE) {
+                if (sinPhi > 0)
+                    leftPow *= -1;
+                else
+                    rightPow *= -1;
+            }*/
+            // Set powers
+            setLeftPow(leftPow);
+            setRightPow(rightPow);
+        }
+        else {
+            setLeftPow(0.0);
+            setRightPow(0.0);
+        }
+    }
+
+    public Spherical3D cartesianToSpherical(Cartesian3D cartesian) {
+        double
+                x2 = cartesian.x * cartesian.x,
+                y2 = cartesian.y * cartesian.y,
+                z2 = cartesian.z * cartesian.z;
+        double r = Math.sqrt(x2 + y2 + z2);
+        double theta = Math.acos(cartesian.z / r) * org.firstinspires.ftc.teamcode.Constants.RADS_TO_DEGS;
+        double phi = Math.atan2(cartesian.y, cartesian.x) * org.firstinspires.ftc.teamcode.Constants.RADS_TO_DEGS;
+        return new Spherical3D(r, theta, phi);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotTestTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBotTestTemplate.java
@@ -129,6 +129,7 @@ public abstract class DriveBotTestTemplate extends OpMode {
     public boolean prox, possibleProx;
 
     protected DistanceSensor dSensorR, dSensorL;
+
     @Override
     public void init() {
         this.msStuckDetectInit = 60000;
@@ -170,7 +171,6 @@ public abstract class DriveBotTestTemplate extends OpMode {
         rIntake = hardwareMap.servo.get("rir");
 
         lIntake = hardwareMap.servo.get("lir");
-
 
 
         glyphOutput = hardwareMap.servo.get("gd");
@@ -266,25 +266,25 @@ public abstract class DriveBotTestTemplate extends OpMode {
         danceMusic = null;
     }
 
-    GlyphInOutIntakeState checkGlyphIntakeStatus(){
+    GlyphInOutIntakeState checkGlyphIntakeStatus() {
         GlyphInOutIntakeState returnState;
 
-        if(!(Double.isNaN(intakeSensorRange.getDistance(DistanceUnit.CM)))){
+        if (!(Double.isNaN(intakeSensorRange.getDistance(DistanceUnit.CM)))) {
             returnState = GlyphInOutIntakeState.INSIDE;
 
-        }else {
+        } else {
             returnState = GlyphInOutIntakeState.OUTSIDE;
         }
         return returnState;
     }
 
     // Returns the change in the number of glyphs in the intake system.
-    int checkGlyphCount(IntakeState paramIntakeState, GlyphInOutIntakeState paramGlyphInOutIntakeState, GlyphInOutIntakeState paramPrevGlyphInOutIntakeState){
+    int checkGlyphCount(IntakeState paramIntakeState, GlyphInOutIntakeState paramGlyphInOutIntakeState, GlyphInOutIntakeState paramPrevGlyphInOutIntakeState) {
 
-        if(!paramGlyphInOutIntakeState.equals(paramPrevGlyphInOutIntakeState)){ //
-            if((paramGlyphInOutIntakeState == GlyphInOutIntakeState.INSIDE) && paramIntakeState == IntakeState.RETRIEVING){
+        if (!paramGlyphInOutIntakeState.equals(paramPrevGlyphInOutIntakeState)) { //
+            if ((paramGlyphInOutIntakeState == GlyphInOutIntakeState.INSIDE) && paramIntakeState == IntakeState.RETRIEVING) {
                 return 1;
-            } else if((paramGlyphInOutIntakeState == GlyphInOutIntakeState.OUTSIDE) && (paramIntakeState == IntakeState.SPITTING)) {
+            } else if ((paramGlyphInOutIntakeState == GlyphInOutIntakeState.OUTSIDE) && (paramIntakeState == IntakeState.SPITTING)) {
                 return -1;
             }
 
@@ -293,12 +293,12 @@ public abstract class DriveBotTestTemplate extends OpMode {
     }
 
     // Returns the current intake state based on the power being provided to the intake motors
-    IntakeState checkIntakeState(double paramPower, IntakeState paramIntakeState){
-        if (paramPower > 0){
+    IntakeState checkIntakeState(double paramPower, IntakeState paramIntakeState) {
+        if (paramPower > 0) {
             return IntakeState.RETRIEVING;
-        }else if (paramPower < 0){
+        } else if (paramPower < 0) {
             return IntakeState.SPITTING;
-        }else{
+        } else {
             return paramIntakeState;
         }
     }
@@ -316,7 +316,6 @@ public abstract class DriveBotTestTemplate extends OpMode {
         belt1.setPower(power);
         belt2.setPower(power);
     }
-
 
 
     protected void scream() {
@@ -509,14 +508,14 @@ public abstract class DriveBotTestTemplate extends OpMode {
         }
     }
 
-    protected static boolean timeReached(long time, long wait){
+    protected static boolean timeReached(long time, long wait) {
         return System.currentTimeMillis() - time >= wait;
     }
 
-    protected boolean runWithArmDistance(DistanceSensor dSensor){
+    protected boolean runWithArmDistance(DistanceSensor dSensor) {
 
-        if(String.format(Locale.US, "%.02f", dSensor.getDistance(DistanceUnit.CM)).equals("NaN")){
-            if(dSensor.getDistance(DistanceUnit.CM) != (double)Double.NaN){
+        if (String.format(Locale.US, "%.02f", dSensor.getDistance(DistanceUnit.CM)).equals("NaN")) {
+            if (dSensor.getDistance(DistanceUnit.CM) != (double) Double.NaN) {
                 return true;
             }
         }
@@ -525,13 +524,14 @@ public abstract class DriveBotTestTemplate extends OpMode {
     }
 
 
-    protected void hitLeftJewel(){
+    protected void hitLeftJewel() {
         jewelFlipper.setPosition(1.0);
     }
 
-    protected void hitRightJewel(){
+    protected void hitRightJewel() {
         jewelFlipper.setPosition(0.0);
     }
+
     // This is here for not loading the gyro sensor when in teleop.
     protected boolean isAutonomous() {
         return true;
