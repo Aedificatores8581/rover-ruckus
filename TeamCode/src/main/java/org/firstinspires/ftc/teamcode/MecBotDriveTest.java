cf2a07d9c00c4e319c06258ecd3d4eb36ecb0477
theZebraBatman <fgportman00@gmail.com>
2018-03-31 21:32:03 -0400

    Added init and start from superclass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecBotDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecBotDriveTest.java
@@ -3,14 +3,16 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
- * Created by fgpor on 3/31/2018.
+ * Created by Frank Portman on 3/31/2018.
  */
 @TeleOp(name = "MecBotTest", group = "MecBot")
 public class MecBotDriveTest extends MecBotTemplate {
     double I, II, III, IV, max, min;
     boolean brake;
+
     @Override
     public void init(){
+        super.init();
         I = 0;
         II = 0;
         III = 0;
@@ -18,29 +20,26 @@ public class MecBotDriveTest extends MecBotTemplate {
         brake = true;
         max = 1;
         min = -1;
+
     }
     @Override
     public void start(){
+        super.init();
 
     }
     @Override
     public void loop(){
         I = gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
-
         II = gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x;
-
         III = gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x;
-
         IV = gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
-
         max = Math.max(Math.max(Math.abs(I), Math.abs(II)), Math.max(Math.abs(III), Math.abs(IV)));
-        if(max > 1){
+        if(max> 1){
             I /= max;
             II /= max;
             III /= max;
             IV /= max;
         }
-
         refreshMotors(I, II, III, IV, true);
     }
 }
