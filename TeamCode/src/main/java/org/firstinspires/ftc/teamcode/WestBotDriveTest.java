d3c51a985e753b36a8e726e600edf537f496e395
theZebraBatman <fgportman00@gmail.com>
2018-04-04 08:40:42 -0400

    Added different drive modes to westBot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestBotDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestBotDriveTest.java
@@ -7,7 +7,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  */
 @TeleOp(name = "westBotTestDrive", group = "Test_Drive")
 public class WestBotDriveTest extends WestBotTemplate {
+    CONTROL_STATE cs;
+    DRIVE_MODE dm;
     int mult = 0;
+    double rt = 0, x = 0, y = 0, b = 0, angle = 0;
+    boolean switchMode = false, switchBool = false;
     @Override
     public void init(){
         super.init();
@@ -18,14 +22,81 @@ public class WestBotDriveTest extends WestBotTemplate {
     }
     @Override
     public void loop() {
-        mult = 0;
-        if(gamepad1.left_stick_y > 0)
-            mult = 1;
-        else if(gamepad1.left_stick_y < 0)
-            mult = -1;
-        setLeftPow((-gamepad1.left_stick_y) - TURN_MULT * (gamepad1.right_stick_x * mult));
-        setRightPow((-gamepad1.left_stick_y) + TURN_MULT * (gamepad1.right_stick_x * mult));
-        brake(1);
+        x = gamepad1.left_stick_x;
+        rt = gamepad1.right_trigger;
+        switch(cs){
+            case ARCADE:
+                mult = 0;
+                if(gamepad1.left_stick_y > 0)
+                    mult = 1;
+                else if(gamepad1.left_stick_y < 0)
+                    mult = -1;
+                b = gamepad1.right_trigger;
+                if(b >= UniversalConstants.Triggered.TRIGGER)
+                    y = b;
+                else {
+                    y = gamepad1.left_stick_y;
+                    y = -Math.sqrt(x * x + y * y) * UniversalFunctions.round(y);
+                }
+                setLeftPow((-gamepad1.left_stick_y) - TURN_MULT * (gamepad1.right_stick_x * mult));
+                setRightPow((-gamepad1.left_stick_y) + TURN_MULT * (gamepad1.right_stick_x * mult));
+                brake(1);
+                if(switchMode) {
+                    cs = cs.FIELD_CENTRIC;
+                    switchMode = false;
+                    switchBool = false;
+                }
+                else if(rt < 0.2){
+                    switchMode = false;
+                    switchBool = true;
+                }
+                else if(rt > 0.2 && switchBool)
+                    switchMode = true;
+                break;
+            case FIELD_CENTRIC:
+                b = gamepad1.right_trigger;
+                if(b >= UniversalConstants.Triggered.TRIGGER)
+                    y = b;
+                else {
+                    y = gamepad1.left_stick_y;
+                    y = -Math.sqrt(x * x + y * y) * UniversalFunctions.round(y);
+                }
+                if(y > 0)
+                    mult = 1;
+                else if(y < 0)
+                    mult = -1;
+                setLeftPow(y - TURN_MULT * (normalizeGamepadAngle(normalizeGyroAngle(getGyroAngle()))));
+                setRightPow(y + TURN_MULT * (normalizeGamepadAngle(normalizeGyroAngle(getGyroAngle()))));
+
+                if(switchMode) {
+                    cs = cs.TANK;
+                    switchMode = false;
+                    switchBool = false;
+                }
+                else if(rt < 0.2){
+                    switchMode = false;
+                    switchBool = true;
+                }
+                else if(rt > 0.2 && switchBool)
+                    switchMode = true;
+                break;
+            case TANK:
+                setLeftPow(-gamepad1.left_stick_y);
+                setRightPow(-gamepad1.right_stick_y);
+                if(switchMode) {
+                    cs = cs.ARCADE;
+                    switchMode = false;
+                    switchBool = false;
+                }
+                else if(rt < 0.2){
+                    switchMode = false;
+                    switchBool = true;
+                }
+                else if(rt > 0.2 && switchBool)
+                    switchMode = true;
+                break;
+        }
+
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestBotTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestBotTemplate.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * Created by Frank Portman on 3/31/2018.
@@ -10,10 +15,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public abstract class WestBotTemplate extends OpMode{
     DcMotor lf, lr, rf, rr;
-    public static final DcMotor.Direction
-            LDIR = DcMotorSimple.Direction.FORWARD,
-            RDIR = DcMotorSimple.Direction.REVERSE;
+    public static final DcMotor.Direction LDIR = DcMotorSimple.Direction.FORWARD, RDIR = DcMotorSimple.Direction.REVERSE;
     public static final double TURN_MULT = 0.75;
+    BNO055IMU imu;
+    GyroAngles gyroangles;
+    Orientation angles;
+    double startAngle;
     public void init(){
         lf = hardwareMap.dcMotor.get("lf");
         lr = hardwareMap.dcMotor.get("lr");
@@ -23,10 +30,43 @@ public abstract class WestBotTemplate extends OpMode{
         lr.setDirection(LDIR);
         rf.setDirection(RDIR);
         rr.setDirection(RDIR);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, GyroAngles.ORDER, GyroAngles.UNIT);
+        gyroangles = new GyroAngles(angles);
     }
 
     public void start(){
-
+        startAngle = getGyroAngle();
+    }
+    public double getGyroAngle(){
+        return gyroangles.refreshGyroAngles(angles);
+        //return gyroSensor.rawX();
+    }
+    public double normalizeGyroAngle(double angle){
+        angle += startAngle;
+        double a2 = Math.abs(angle) %  360;
+        if(Math.abs(angle) != angle){
+            return 360 - a2;
+        }
+        return a2;
+    }
+    protected double normalizeGamepadAngle(double angle){
+        return UniversalFunctions.normalizeAngle(getGamepadAngle(), angle);
+    }
+    protected double getGamepadAngle(){
+        double x = gamepad1.right_stick_x;
+        double y = gamepad1.right_stick_y;
+        return (UniversalFunctions.round(y) / 2 + 0.5 * Math.abs(y)) * 180 + Math.toDegrees(Math.acos(x / (Math.sqrt(x * x + y * y))));
     }
     public void setLeftPow(double pow){
         lf.setPower(pow);
@@ -36,8 +76,21 @@ public abstract class WestBotTemplate extends OpMode{
         rf.setPower(pow);
         rr.setPower(pow);
     }
-    public void brake(int dir){
-        setLeftPow(0.05 * dir);
-        setRightPow(0.05 * dir);
+    public boolean brake(int dir){
+        if(lf.getPower() == 0 && rf.getPower() == 0 && rr.getPower() == 0 &&rf.getPower() == 0) {
+            setLeftPow(0.05 * dir);
+            setRightPow(0.05 * dir);
+            return true;
+        }
+        return false;
+    }
+    public enum CONTROL_STATE{
+        ARCADE,
+        TANK,
+        FIELD_CENTRIC,
+    }
+    public enum DRIVE_MODE{
+        TRIGGER,
+        Y_AX,
     }
 }
