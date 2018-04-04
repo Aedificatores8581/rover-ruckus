17bca7afca7a44c11ea114caed1d07bc8f848af5
theZebraBatman <fgportman00@gmail.com>
2018-04-04 08:48:47 -0400

    Created new constants class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Frank Portman on 4/3/2018.
+ */
+
+public final class UniversalConstants {
+
+    public static final class Triggered{
+        public static final double STICK = 0.2;
+        public static final double TRIGGER = 0.2;
+    }
+}
