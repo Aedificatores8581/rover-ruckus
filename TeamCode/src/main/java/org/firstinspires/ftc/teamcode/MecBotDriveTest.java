ec321687ef4fd5af172edc545731a74b653d3b55
theZebraBatman <fgportman00@gmail.com>
2018-04-05 12:49:38 -0400

    Added Feild-centric option for MecBot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecBotDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecBotDriveTest.java
@@ -7,9 +7,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  */
 @TeleOp(name = "MecBotTestDrive", group = "Test_Drive")
 public class MecBotDriveTest extends MecBotTemplate {
-    double I, II, III, IV, max, min, x, y;
+    double I, II, III, IV, max, min, x, y, mult, rt;
     boolean brake;
-
+    ControlState cs;
+    DriveMode dm;
+    boolean switchMode = false, switchBool = false;
     @Override
     public void init(){
         super.init();
@@ -20,29 +22,62 @@ public class MecBotDriveTest extends MecBotTemplate {
         brake = true;
         max = 1;
         min = -1;
-
+        mult = 0;
+        rt = 0;
     }
     @Override
     public void start(){
-        super.init();
-
+        super.start();
     }
     @Override
     public void loop(){
         x = gamepad1.left_stick_x;
         y = gamepad1.left_stick_y;
-        x = Math.sqrt(x * x + y * y) * round(x);
-        y = Math.sqrt(x * x + y * y) * round(y);
-        I = gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
-        II = gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x;
-        III = gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x;
-        IV = gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
+        x = Math.sqrt(x * x + y * y) * UniversalFunctions.round(x);
+        y = Math.sqrt(x * x + y * y) * UniversalFunctions.round(y);
+        rt = gamepad1.right_trigger;
+        switch(cs){
+            case ARCADE:
+                I = y - x - gamepad1.right_stick_x;
+                II = y + x + gamepad1.right_stick_x;
+                III = y - x + gamepad1.right_stick_x;
+                IV = y + x - gamepad1.right_stick_x;
+                if(switchMode) {
+                    cs = cs.FIELD_CENTRIC;
+                    switchMode = false;
+                    switchBool = false;
+                }
+                else if(rt < 0.2){
+                    switchMode = false;
+                    switchBool = true;
+                }
+                else if(rt > 0.2 && switchBool)
+                    switchMode = true;
+                break;
+            case FIELD_CENTRIC:
+                mult = Math.cos(normalizeGamepadAngle(normalizeGyroAngle(getGyroAngle())));
+                I = y - x - mult;
+                II = y + x + mult;
+                III = y - x + mult;
+                IV = y + x - mult;
+                if(switchMode) {
+                    cs = cs.ARCADE;
+                    switchMode = false;
+                    switchBool = false;
+                }
+                else if(rt < 0.2){
+                    switchMode = false;
+                    switchBool = true;
+                }
+                else if(rt > 0.2 && switchBool)
+                    switchMode = true;
+                break;
+        }
         max = Math.max(Math.max(Math.abs(I), Math.abs(II)), Math.max(Math.abs(III), Math.abs(IV)));
         I = I * y/ max;
         II = II * y  / max;
         III = III *  y/ max;
         IV = IV * y / max;
-
         refreshMotors(I, II, III, IV, true);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecBotTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecBotTemplate.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.UniversalFunctions;
 
 /**
  * Created by Frank Portman on 3/31/2018.
@@ -10,6 +16,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public abstract class MecBotTemplate extends OpMode{
     DcMotor lf, lr, rf, rr;
+    double startAngle;
+    BNO055IMU imu;
+    GyroAngles gyroangles;
+    Orientation angles;
     private static final DcMotor.Direction
             LDIR = DcMotorSimple.Direction.FORWARD,
             RDIR = DcMotorSimple.Direction.REVERSE;
@@ -25,8 +35,22 @@ public abstract class MecBotTemplate extends OpMode{
         rf.setDirection(RDIR);
         rr.setDirection(RDIR);
 
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, GyroAngles.ORDER, GyroAngles.UNIT);
+        gyroangles = new GyroAngles(angles);
+
     }
     public void start(){
+        startAngle = getGyroAngle();
 
     }
     public void refreshMotors(double I, double II, double III, double IV, boolean brake){
@@ -55,10 +79,33 @@ public abstract class MecBotTemplate extends OpMode{
             lf.setPower(BRAKE_POW);
         }
     }
-    protected double round(double d){
-        if(d < 0){
-            return Math.floor(d);
+
+    public enum ControlState{
+        ARCADE,
+        FIELD_CENTRIC,
+    }
+    public enum DriveMode{
+        TANK,
+        ELSE,
+    }
+    protected double normalizeGamepadAngle(double angle){
+        return UniversalFunctions.normalizeAngle(getGamepadAngle(), angle);
+    }
+    protected double getGamepadAngle(){
+        double x = gamepad1.right_stick_x;
+        double y = gamepad1.right_stick_y;
+        return (UniversalFunctions.round(y) / 2 + 0.5 * Math.abs(y)) * 180 + Math.toDegrees(Math.acos(x / (Math.sqrt(x * x + y * y))));
+    }
+    public double getGyroAngle(){
+        return gyroangles.refreshGyroAngles(angles);
+        //return gyroSensor.rawX();
+    }
+    public double normalizeGyroAngle(double angle){
+        angle += startAngle;
+        double a2 = Math.abs(angle) %  360;
+        if(Math.abs(angle) != angle){
+            return 360 - a2;
         }
-        return Math.ceil(d);
+        return a2;
     }
 }
