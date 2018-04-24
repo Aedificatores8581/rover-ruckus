ee64cad4abe8a4967053961ee0ff50dbe9898e07
theZebraBatman <fgportman00@gmail.com>
2018-04-23 20:23:56 -0400

    Added maximum functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalFunctions.java
@@ -19,6 +19,13 @@ public abstract class UniversalFunctions {
         }
         return a2;
     }
+    protected static double normalizeAngle(double angle) {
+        double a2 = Math.abs(angle) % 360;
+        if (Math.abs(angle) != angle) {
+            return 360 - a2;
+        }
+        return a2;
+    }
     protected static double normalizeAngle180(double angle, double newStartAngle) {
         double ang = normalizeAngle(angle, newStartAngle);
         if(ang > 180){
@@ -26,5 +33,47 @@ public abstract class UniversalFunctions {
         }
         return ang;
     }
-
+    protected static double normalizeAngle180(double angle) {
+        double ang = normalizeAngle(angle);
+        if(ang > 180){
+            ang = -180 + ang % 360;
+        }
+        return ang;
+    }
+    protected static double max(double a, double b, double c){
+        return Math.max(Math.max(a, b), c);
+    }
+    protected static double max(double a, double b, double c, double d){
+        return Math.max(max(a, b, c), d);
+    }
+    protected static double max(double a, double b, double c, double d, double e){
+        return Math.max(max(a, b, c, d), e);
+    }
+    protected static double max(double a, double b, double c, double d, double e, double f){
+        return Math.max(max(a, b, c, d, e), f);
+    }
+    protected static double max(double a, double b, double c, double d, double e, double f, double g){
+        return Math.max(max(a, b, c, d, e, f), g);
+    }
+    protected static double max(double a, double b, double c, double d, double e, double f, double g, double h){
+        return Math.max(max(a, b, c, d, e, f, g), h);
+    }
+    protected static double maxAbs(double a, double b, double c){
+        return Math.max(Math.max(Math.abs(a), Math.abs(b)), Math.abs(c));
+    }
+    protected static double maxAbs(double a, double b, double c, double d){
+        return Math.max(maxAbs(a, b, c), Math.abs(d));
+    }
+    protected static double maxAbs(double a, double b, double c, double d, double e){
+        return Math.max(maxAbs(a, b, c, d), Math.abs(e));
+    }
+    protected static double maxAbs(double a, double b, double c, double d, double e, double f){
+        return Math.max(maxAbs(a, b, c, d, e), Math.abs(f));
+    }
+    protected static double maxAbs(double a, double b, double c, double d, double e, double f, double g){
+        return Math.max(maxAbs(a, b, c, d, e, f), Math.abs(g));
+    }
+    protected static double maxAbs(double a, double b, double c, double d, double e, double f, double g, double h){
+        return Math.max(maxAbs(a, b, c, d, e, f, g), Math.abs(h));
+    }
 }
