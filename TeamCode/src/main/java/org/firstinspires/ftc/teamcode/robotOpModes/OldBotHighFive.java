050c95e0fd12aa81066613916092d378a59c4f09
TheSaminator <samfrancis00@gmail.com>
2018-05-06 16:06:55 -0400

    Improve the high-five opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotOpModes/OldBotHighFive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotOpModes/OldBotHighFive.java
@@ -10,9 +10,20 @@ import org.firstinspires.ftc.teamcode.robotTemplates.OldBotTemplate;
 public class OldBotHighFive extends OldBotTemplate {
     final double JEWEL_HAND_LENGTH = 9.6;
     final double JEWEL_HAND_THRESHOLD = 8;
+    final double JEWEL_ARM_POSITION = 0.36;
+    final double RADS_TO_DEGS = 180 / Math.PI;
+    final double SPEED_MULTIPLIER = 0.42;
+
+    @Override
+    public void start() {
+        jewelArm.setPosition(0.36);
+    }
 
     @Override
     public void loop() {
+        setLeftPow(gamepad1.left_stick_y * SPEED_MULTIPLIER);
+        setRightPow(gamepad1.right_stick_y * SPEED_MULTIPLIER);
+
         // Jewel hand length is 9.6 cm.
         if (dSensorL.getDistance(DistanceUnit.CM) < JEWEL_HAND_THRESHOLD || dSensorR.getDistance(DistanceUnit.CM) < JEWEL_HAND_THRESHOLD) {
             double mult = 1;
@@ -20,13 +31,21 @@ public class OldBotHighFive extends OldBotTemplate {
             if (dSensorL.getDistance(DistanceUnit.CM) < dSensorR.getDistance(DistanceUnit.CM)) {
                 mult *= -1;
                 distance = dSensorL.getDistance(DistanceUnit.CM);
-            }
-            else distance = dSensorR.getDistance(DistanceUnit.CM);
+            } else distance = dSensorR.getDistance(DistanceUnit.CM);
 
             double angle = Math.acos(distance / JEWEL_HAND_LENGTH);
             double position = angle / Math.PI;
 
             jewelFlipper.setPosition(position * mult);
+
+            telemetry.addData("Angle", angle * RADS_TO_DEGS);
+            telemetry.addData("Distance", distance);
+            telemetry.addData("Position", position);
         }
+        telemetry.addData("L Distance", dSensorL.getDistance(DistanceUnit.CM));
+        telemetry.addData("R Distance", dSensorR.getDistance(DistanceUnit.CM));
+
+        telemetry.addData("L Speed", gamepad1.left_stick_y * SPEED_MULTIPLIER);
+        telemetry.addData("R Speed", gamepad1.right_stick_y * SPEED_MULTIPLIER);
     }
 }
