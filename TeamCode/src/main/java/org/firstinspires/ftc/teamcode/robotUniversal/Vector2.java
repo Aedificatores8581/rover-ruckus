c7a7cfdb9a77a1f4d9afae18de3ca6e6d1842553
vzyrianov <vzyriano@kent.edu>
2018-05-19 21:01:58 -0400

    Implementing more functions in Vector2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/Vector2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/Vector2.java
@@ -19,19 +19,46 @@ public class Vector2 {
         this.y = y;
     }
 
+    //Sets the vector to equal the cartesian equivalent of the polar vector composed of a length and an angle
+    public void setFromPolar(double r, double theta) {
+        this.x = Math.cos(theta) * r;
+        this.y = Math.sin(theta) * r;
+    }
+
+    public void add(Vector2 vector) {
+        x += vector.x;
+        y += vector.y;
+    }
+
+    public void subtract(Vector2 vector) {
+        vector.scalarMultiply(-1);
+        add(vector);
+    }
+
+    public void scalarMultiply(double a) {
+        x *= a;
+        y *= a;
+    }
+
     //Length of vector
     public double magnitude() {
         return Math.sqrt(x*x + y*y);
     }
 
     //Dot Product
-    public double dot(Vector2 vector2) {
-        return vector2.x * this.x + vector2.y * this.y;
+    public double dot(Vector2 vector) {
+        return vector.x * this.x + vector.y * this.y;
     }
 
     //Angular component of the vector if it was converted to polar coordinates
+    //Returns angle in radians
     public double angle() {
         return Math.atan2(y, x);
+    }
+
+    //Returns angle between two vectors in radians.
+    public double angleBetween(Vector2 vector) {
+        return Math.acos(this.dot(vector) / (this.magnitude() * vector.magnitude()));
     }
 
     //Copied from Point class.
