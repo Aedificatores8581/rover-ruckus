f5f7596c51315c1bb0df2f71208e303d388aa44a
theZebraBatman <fgportman00@gmail.com>
2018-05-19 21:03:50 -0400

    Added refreshGyroAnglesRadians function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/GyroAngles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/GyroAngles.java
@@ -37,10 +37,13 @@ public class GyroAngles {
         z = AngleUnit.DEGREES.fromUnit(UNIT, angles.firstAngle);
         return z;
     }
+    public double refreshGyroAnglesRadians(Orientation angles){
+        z = AngleUnit.RADIANS.fromUnit(AngleUnit.RADIANS, angles.firstAngle);
+        return z;
+    }
     public static String formatAngle(AngleUnit angleUnit, double angle) {
         return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
     }
-
     public static String formatDegrees(double degrees) {
         return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
