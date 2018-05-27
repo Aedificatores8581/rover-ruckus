476d03c2cac936e20d9fc4e30ec4a97715875a43
theZebraBatman <fgportman00@gmail.com>
2018-05-27 12:16:46 -0400

    Added methods and comments to MecanumDT and updated Mecanum2 loop method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/Mecanum2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/Mecanum2.java
@@ -1,56 +1,43 @@
 package org.firstinspires.ftc.teamcode.robotTemplates;
 
 import org.firstinspires.ftc.teamcode.robotUniversal.UniversalFunctions;
+import org.firstinspires.ftc.teamcode.robotUniversal.Vector2;
 
 /**
  * Created by Frank Portman on 5/21/2018
  */
 public class Mecanum2 extends MecanumDT {
-    double angle, y, x, rad, rt, rx, max, I, II, III, IV, speed;
-    double turnMult;
+    double turnMult, angleBetween;
     ControlState cs;
     public Mecanum2(double brakePow, double sped) {
         super(brakePow);
         speed = sped;
     }
-
     @Override
     public void init() {
-
+        super.init();
     }
-
     @Override
-    public String[] names() {
-        String[] names = {"rf", "lf", "lr", "rr"};
-        return names;
+    public void start(){
+        super.start();
     }
-
     @Override
     public void loop() {
-        x = gamepad1.left_stick_x;
-        y = gamepad1.left_stick_y;
-        rt = gamepad1.right_trigger;
-        angle = Math.toRadians(normalizeGamepadAngleL(normalizeGyroAngle()));
-        y = Math.cos(angle) * rad;
-        x = Math.sin(angle) * rad;
+        updateGamepad1();
+        setGyroAngle();
+        angleBetween = lStick1.angleBetween(robotAngle);
+        setRightForePow(angleBetween, lStick1.magnitude());
+        setLeftForePow(angleBetween, lStick1.magnitude());
         switch (cs) {
             case ARCADE:
-                turnMult = 1 - rad * (1 - super.turnMult);
-                rx = gamepad1.right_stick_x * turnMult;
+                turnMult = 1 - lStick1.magnitude() * (1 - super.turnMult);
+                setTurn(rStick1.magnitude() * turnMult);
                 break;
             case FIELD_CENTRIC:
-                rx = Math.sin(normalizeGamepadAngleR(normalizeGyroAngle()));
+                setTurn(Math.sin(rStick1.angleBetween(robotAngle)));
                 break;
         }
-        I = y - x - rx;
-        II = y + x + rx;
-        III = y - x + rx;
-        IV = y + x - rx;
-        max = UniversalFunctions.maxAbs(I, II, III, IV);
-        I = I * y / max;
-        II = II * y / max;
-        III = III * y / max;
-        IV = IV * y / max;
-        refreshMotors(I, II, III, IV, speed);
+        normalizeMotorPow();
+        refreshMotors();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/MecanumDT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/MecanumDT.java
@@ -20,6 +20,7 @@ import org.firstinspires.ftc.teamcode.robotUniversal.Vector2;
 public abstract class MecanumDT extends Drivetrain {
     DcMotor lf, lr, rf, rr;
     double turnMult = 1;
+    public double leftForePow, rightForePow, turnPow;
     public MecanumDT(double brakePow){
         super(brakePow);
     }
@@ -29,29 +30,96 @@ public abstract class MecanumDT extends Drivetrain {
     public void start(){
         super.start();
     }
-    public double getRFLRPower(double ang, double speed){
-        return Math.sin(Math.toRadians(ang)) * speed / 2;
+
+    //returns the power of the left rear and right fore motors needed to drive at a given angle at a given speed
+    public double getRightForePow(double ang, double speed){
+        return Math.sin(ang) * speed / 2;
     }
-    public double getRFLRPower(Vector2 vel){
-        return getRFLRPower(vel.angle(), vel.magnitude());
+
+    //returns the power of the left rear and right fore motors needed to drive along a given vector
+    public double getRightForePow(Vector2 vel){
+        return getRightForePow(vel.angle(), vel.magnitude());
     }
-    public double getLFRRPower(double ang, double speed){
-        return Math.cos(Math.toRadians(ang)) * speed / 2;
+
+    //returns the power of the left fore and right rear motors needed to drive at a given angle at a given speed
+    public double getLeftForePow(double ang, double speed){
+        return Math.cos(ang) * speed / 2;
     }
-    public double getLFRRPower(Vector2 vel){
-        return getRFLRPower(vel.angle(), vel.magnitude());
+    //returns the power of the left fore and right rear motors needed to drive along a given vector
+    public double getLeftForePow(Vector2 vel){
+        return getLeftForePow(vel.angle(), vel.magnitude());
     }
+    //sets the power of the motors in order to drive at a given angle at a given speed
     public void setDirection(double ang, double speed){
-        double rflr = getRFLRPower(ang, speed);
-        double lfrr = getLFRRPower(ang, speed);
-        rf.setPower(rflr);
-        lf.setPower(rflr);
-        lf.setPower(lfrr);
-        rr.setPower(lfrr);
+        rightForePow = getRightForePow(ang, speed);
+        leftForePow= getLeftForePow(ang, speed);
     }
+    //sets the power of the motors in order to drive at a given velocity
     public void setDirection(Vector2 vel){
         setDirection(vel.angle(), vel.magnitude());
     }
+    //sets the power of the left fore and right rear motors
+    public void setLeftForePow(double pow){
+        leftForePow = pow;
+    }
+    //sets the power of the left fore and right rear motors in order to drive at a given angle at a given speed
+    public void setLeftForePow(double ang, double speed){
+        setLeftForePow(getLeftForePow(ang, speed));
+    }
+    //sets the direction of the right fore and left rear motors
+    public void setRightForePow(double pow){
+        rightForePow = pow;
+    }
+    //sets the direction of the right fore and left rear motors in order to drive at a given angle at a given speed
+    public void setRightForePow(double ang, double speed){
+        setRightForePow(getLeftForePow(ang, speed));
+    }
+    //sets the direction of the motors in order to drive at a given velocity
+    public void setRightForePow(Vector2 vel){
+        setRightForePow(vel.angle(), vel.magnitude());
+    }
+    //sets the turnPow variable
+    public void setTurn(double pow){
+        pow *= turnMult;
+        turnPow = pow;
+    }
+    //sets the motor powers to the specified values
+    public void refreshMotors(double I, double II, double III, double IV){
+        rf.setPower(I);
+        lf.setPower(II);
+        lr.setPower(III);
+        rr.setPower(IV);
+    }
+    //sets the motor powers to the specified values at the specified speed
+    public void refreshMotors(double I, double II, double III, double IV, double speed){
+        rf.setPower(speed * I);
+        lf.setPower(speed * II);
+        lr.setPower(speed * III);
+        rr.setPower(speed * IV);
+    }
+    //sets the motor powers to rightForePow and leftForePow respectively
+    public void refreshMotors(){
+        setPower(rf, rightForePow - turnPow);
+        setPower(lr, rightForePow + turnPow);
+        setPower(lf, leftForePow + turnPow);
+        setPower(rr, leftForePow - turnPow);
+    }
+    //sets the turnMult variable
+    public void setTurnMult(double tm){
+        turnMult = tm;
+    }
+    //normalizes the motor powers to never go above 1 or below -1
+    public void normalizeMotorPow(){
+        double max = UniversalFunctions.maxAbs(leftForePow, rightForePow);
+        leftForePow /= max;
+        rightForePow /= max;
+    }
+
+    protected enum ControlState{
+        ARCADE,
+        FIELD_CENTRIC,
+    }
+
     @Override
     public String[] names(){
         String[] names = {"rf", "lf", "lr", "rr"};
@@ -67,25 +135,6 @@ public abstract class MecanumDT extends Drivetrain {
     public DcMotor.Direction[] dir(){
         DcMotor.Direction[] dir = {DcMotor.Direction.FORWARD, DcMotor.Direction.REVERSE, DcMotor.Direction.REVERSE, DcMotor.Direction.FORWARD};
         return dir;
-    }
-    public void refreshMotors(double I, double II, double III, double IV){
-        rf.setPower(I);
-        lf.setPower(II);
-        lr.setPower(III);
-        rr.setPower(IV);
-    }
-    public void refreshMotors(double I, double II, double III, double IV, double speed){
-        rf.setPower(speed * I);
-        lf.setPower(speed * II);
-        lr.setPower(speed * III);
-        rr.setPower(speed * IV);
-    }
-    protected enum ControlState{
-        ARCADE,
-        FIELD_CENTRIC,
-    }
-    public void setTurnMult(double tm){
-        turnMult = tm;
     }
 
 }
