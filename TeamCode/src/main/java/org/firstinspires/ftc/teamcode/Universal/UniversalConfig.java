183820c0d262ec52aa442b21e8414d745b645436
mayadapples <tlovinski@outlook.com>
2018-12-29 12:29:42 -0500

    Didn't add anything, whoops.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/UniversalConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/UniversalConfig.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.Universal;
 
-public class UniversalConfig {
+/**
+    Written by Theodore Lovinski, 29/12/2018
+
+    TODO FINISH {tbr}
+ */
+
+public interface UniversalConfig {
+    // More to be added, not a priority at the moment.
+    String LIFT_TOP_RATCHET = "lrat";
+    String LIFT_SIDE_RATCHET = "srat";
 }
