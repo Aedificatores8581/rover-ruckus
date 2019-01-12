766a14fd8375d1a126d7b3736ba26402465cfab8
theZebraBatman <fgportman00@gmail.com>
2019-01-12 18:09:22 -0500

    Refractored RealExpression2 to RealNonLinearExpression

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Math/Expressions/RealNonLinearExpression2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Math/Expressions/RealNonLinearExpression2.java
@@ -2,9 +2,9 @@ package org.firstinspires.ftc.teamcode.Universal.Math.Expressions;
 
 import org.opencv.core.Point;
 
-public interface RealExpression2 {
+interface RealNonLinearExpression2 {
     double[] f(double x);
-    double derivative(Point point);
+    double[] derivative(Point point);
     double integral(Point point);
     boolean insideFunction(Point point);
 }
